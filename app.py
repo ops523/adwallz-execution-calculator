d6e653539440d948382de15d93b55cd6f7b76a4c
ops523 <ops@adwallz.com>
2026-04-07 12:18:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from datetime import timedelta
 import math
 
+st.image("logo.png", width=200)
+st.markdown("<h2 style='text-align: center;'>ADWALLZ - On Field Execution Calculator</h2>", unsafe_allow_html=True)
+st.markdown("---")
+
 st.set_page_config(page_title="ADWALLZ - On Field Execution Calculator", layout="centered")
 
 st.title("ADWALLZ - On Field Execution Calculator")
